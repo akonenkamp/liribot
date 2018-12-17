1422949e46c316d15447093c36bad598401cc57c
Konenkamp <n0267144@lm.lmig.com>
2018-12-16 22:55:46 -0800

    adding case statements for bands in town, spotify, etc

--- bot.js
+++ bot.js
@@ -1,5 +1,28 @@
 require("dotenv").config();
 var spotify = new Spotify(keys.spotify);
+var request = require('request');
+var bandsintown = require('bandsintown')(APP_ID);
+var omdb = require('omdb');
+var fs = require('fs');
+
+switch (action) {
+
+    case "spotify-this-song":
+    spotify(inputs);
+    break;
+
+    case "movie-this":
+    omdb(inputs);
+    break;
+
+    case "concert-this":
+    bandsintown(input);
+    break;
+
+    case "do-what-it-says":
+    request(inputs);
+    break;
+};
 
 
 
